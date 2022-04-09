6598ff6cf5b30b5743529bd74d2cc530d5936c07
Erick Castilho Mestre <erickcastilhomestre@gmail.com>
2022-04-09 12:04:16 -0300

    implementation CD

---
+++ src/components/ChannelData/index.tsx
@@ -0,0 +1,109 @@
+//ChannelData
+import React from 'react';
+import { Container, Messages, InputWrapper, Input, InputIcon } from './styles'
+import ChannelMessage, {Mention} from '../ChannelMessage'
+
+
+const ChannelData: React.FC = () => {
+    return (
+        <Container>
+            <Messages>
+                <ChannelMessage
+                    author='Erick Castilho'
+                    date='25/06/2022'
+                    content= 'Hoje é Meu aniverssário'
+                />
+                <ChannelMessage
+                    author='Fulano'
+                    date='25/06/2022'
+                    content= 'Isto é uma mensagem'
+                />
+                <ChannelMessage
+                    author='Fulano'
+                    date='25/06/2022'
+                    content= 'Isto é uma mensagem'
+                />
+                <ChannelMessage
+                    author='Fulano'
+                    date='25/06/2022'
+                    content= 'Isto é uma mensagem'
+                />
+                <ChannelMessage
+                    author='Fulano'
+                    date='25/06/2022'
+                    content= 'Isto é uma mensagem'
+                />
+                <ChannelMessage
+                    author='Fulano'
+                    date='25/06/2022'
+                    content= 'Isto é uma mensagem'
+                />
+                <ChannelMessage
+                    author='Spotify'
+                    date='25/06/2022'
+                    content= 'Tocando => What You Won`t Do For Love - Bobby Caldwell'
+                    isBot
+                />
+                <ChannelMessage
+                    author='Fulano'
+                    date='25/06/2022'
+                    content= 'Isto é uma mensagem'
+                />
+                <ChannelMessage
+                    author='Fulano'
+                    date='25/06/2022'
+                    content= 'Isto é uma mensagem'
+                />
+                <ChannelMessage
+                    author='Fulano'
+                    date='25/06/2022'
+                    content= 'Isto é uma mensagem'
+                />
+                <ChannelMessage
+                    author='Renan Santana'
+                    date='25/06/2022'
+                    content= {
+                        <>
+                        <Mention>@Erick Castilho</Mention>, me carrega no lolzinho ai por favor?
+                        </>
+                        
+                    }
+                    hasMention
+                />
+                <ChannelMessage
+                    author='Fulano'
+                    date='25/06/2022'
+                    content= 'Isto é uma mensagem'
+                />
+                <ChannelMessage
+                    author='Fulano'
+                    date='25/06/2022'
+                    content= 'Isto é uma mensagem'
+                />
+                <ChannelMessage
+                    author='Fulano'
+                    date='25/06/2022'
+                    content= 'Isto é uma mensagem'
+                />
+                <ChannelMessage
+                    author='Fulano'
+                    date='25/06/2022'
+                    content= 'Isto é uma mensagem'
+                />
+                <ChannelMessage
+                    author='Fulano'
+                    date='25/06/2022'
+                    content= 'Isto é uma mensagem'
+                />
+
+
+            </Messages>
+            <InputWrapper>
+                <Input type='text' placeholder='Conversar em #chat-livre'/>
+                <InputIcon />
+            </InputWrapper>
+        </Container>
+    )
+};
+
+export default ChannelData;
